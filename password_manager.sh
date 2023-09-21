d4311c55651fb8de6f81f603768d88f6b4cf77ac
haga_hiroki <ragna.1123@gmail.com>
2023-09-21 23:06:49 +0900

    while文の追加

--- password_manager.sh
+++ password_manager.sh
@@ -1,22 +1,25 @@
 echo "パスワードマネージャーへようこそ！"
 echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
+while :
+do
 read select_mode
 case "$select_mode" in 
-"Add Password")
-	echo "サービス名を入力してください:"
-	read service
-	echo "ユーザー名を入力してください:"
-	read user
-	echo "パスワードを入力してください:"
-	read password
-	echo $service:$user:$password >> password.txt
-	echo "パスワードの追加は成功しました";;
-"Get Password")
-	echo "サービス名を入力してくだい:"
-	read service;;
-"Exit")
-	echo "Thank you!"
-	exit 0;;
-*)
+	"Add Password")
+		echo "サービス名を入力してください:"
+		read service
+		echo "ユーザー名を入力してください:"
+		read user
+		echo "パスワードを入力してください:"
+		read password
+		echo $service:$user:$password >> password.txt
+		echo "パスワードの追加は成功しました";;
+	"Get Password")
+		echo "サービス名を入力してくだい:"
+		read service;;
+	"Exit")
+		echo "Thank you!"
+		exit 0;;
+	*)
 	echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。";;
-esac
+	esac
+done
