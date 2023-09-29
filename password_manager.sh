baa76844a3dd8c292582a2564222ca10c49d9a3a
haga_hiroki <ragna.1123@gmail.com>
2023-09-29 15:03:05 +0900

    パスワード取得時に一時的にテキストへ変換される処理を変数へ格納するよう修正　パスワード複数取得時に改行を入れました

--- password_manager.sh
+++ password_manager.sh
@@ -10,16 +10,19 @@ while :; do
 
 	case "$inp_select_mode" in
 	"Add Password")
-		# 追記するために暗号ファイルを復号
-		gpg -o $FILE -d $ENC_FILE
 		echo "サービス名を入力してください:"
 		read service
 		echo "ユーザー名を入力してください:"
 		read user
 		echo "パスワードを入力してください:"
 		read password
+
+		# 追記するために暗号ファイルを復号
+		gpg -o $FILE -d $ENC_FILE
 		echo "$service:$user:$password" >>$FILE
 		echo "パスワードの追加は成功しました"
+		# 上書きをするために更新前の暗号化ファイルは削除
+		rm $ENC_FILE
 		# 再暗号化とテキストファイル削除
 		gpg -r $USER_NAME -eao $ENC_FILE $FILE
 		rm $FILE
@@ -28,29 +31,26 @@ while :; do
 		;;
 
 	"Get Password")
-		gpg -o $FILE -d $ENC_FILE
+		decrypted_data=$(gpg -d $ENC_FILE)
 		echo "サービス名を入力してくだい:"
 		read inp_service_name
 
 		# 登録件数のフラグ管理値　0なら該当なし
 		service_found=0
 
-		while read lines; do
-			read -r service user password <<<"$lines"
-			IFS=:
+		while IFS=: read -r service user password; do
 			if [ "$inp_service_name" = "$service" ]; then
 				echo "サービス名:$service"
 				echo "ユーザー名:$user"
-				echo "パスワード:$password"
+				echo "パスワード:$password\n"
 				service_found=$(expr $service_found + 1)
 			fi
-		done <$FILE
+		done <<<"$decrypted_data"
 
 		if [ "$service_found" -eq 0 ]; then
 			echo "そのサービスは登録されていません。"
 		fi
-		
-		rm $FILE
+
 		echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
 		;;
 
