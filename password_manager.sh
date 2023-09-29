951363b0a298cdee103a20ef7ddfa13ef15ff804
haga_hiroki <ragna.1123@gmail.com>
2023-09-29 13:35:13 +0900

    暗号化と復号処理の実装

--- password_manager.sh
+++ password_manager.sh
@@ -1,10 +1,17 @@
 echo "パスワードマネージャーへようこそ！"
 echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
 while :; do
+
 	FILE=password.txt
+	ENC_FILE=encrypted_file.txt.gpg
+	USER_NAME=haga
+
 	read inp_select_mode
+
 	case "$inp_select_mode" in
 	"Add Password")
+		# 追記するために暗号ファイルを復号
+		gpg -o $FILE -d $ENC_FILE
 		echo "サービス名を入力してください:"
 		read service
 		echo "ユーザー名を入力してください:"
@@ -13,14 +20,19 @@ while :; do
 		read password
 		echo "$service:$user:$password" >>$FILE
 		echo "パスワードの追加は成功しました"
+		# 再暗号化とテキストファイル削除
+		gpg -r $USER_NAME -eao $ENC_FILE $FILE
+		rm $FILE
+
 		echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
 		;;
 
 	"Get Password")
+		gpg -o $FILE -d $ENC_FILE
 		echo "サービス名を入力してくだい:"
 		read inp_service_name
 
-		# 登録件数のフラグ管理
+		# 登録件数のフラグ管理値　0なら該当なし
 		service_found=0
 
 		while read lines; do
@@ -37,6 +49,8 @@ while :; do
 		if [ "$service_found" -eq 0 ]; then
 			echo "そのサービスは登録されていません。"
 		fi
+		
+		rm $FILE
 		echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
 		;;
 
